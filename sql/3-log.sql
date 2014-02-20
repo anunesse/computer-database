00c66a28372e32199ea2d991ead0922e9f77e455
anunesse <anunesse@excilys.com>
2014-02-20 16:16:11 +0100

    maven computer db

--- sql/3-log.sql
+++
@@ -1,7 +0,0 @@
-create table log (
-  id                        bigint not null auto_increment,
-  optype                      varchar(10),
-  opdate                timestamp,
-  description              varchar(255)
-  constraint pk_log primary key (id))
-;

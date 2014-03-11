7e0cad0ae1a6726206105aa2c8a784d3bb5c82f1
Alexandre Nunesse <anunesse@excilys.com>
2014-03-11 18:10:44 +0100

    Implement Criteria API v3.2.0

--- ComputerDB-persistence/src/main/java/com/excilys/formation/projet/dao/impl/ComputerDAO.java
+++ ComputerDB-persistence/src/main/java/com/excilys/formation/projet/dao/impl/ComputerDAO.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.formation.projet.dao.IComputerDAO;
+import com.excilys.formation.projet.om.Company;
 import com.excilys.formation.projet.om.Computer;
 
 @Repository
@@ -62,12 +69,31 @@ public class ComputerDAO implements IComputerDAO {
 	 * @return
 	 */
 	public List<Computer> read(int limit, int offset, String type, String field, String search) {
-		String order = "ORDER BY "+type+" "+field;
-		return entityManager.createQuery("SELECT computer FROM Computer as computer LEFT JOIN computer.company company WITH company.name LIKE :search WHERE computer.name LIKE :search " + order)
-				.setParameter("search", "%" + search + "%")
-				.setFirstResult(offset)
-				.setMaxResults(limit)
-				.getResultList();
+//		String order = "ORDER BY "+type+" "+field;
+//		return entityManager.createQuery("SELECT computer FROM Computer as computer LEFT JOIN computer.company company WITH company.name LIKE :search WHERE computer.name LIKE :search " + order)
+//				.setParameter("search", "%" + search + "%")
+//				.setFirstResult(offset)
+//				.setMaxResults(limit)
+//				.getResultList();
+		
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<Computer> computer = cq.from(Computer.class);
+		cq.select(cb.count(computer));
+		
+		Join<Computer, Company> company = computer.join("company", JoinType.LEFT);
+		String order = new StringBuilder("%").append("ORDER BY ").append(type).append(" ").append(field).toString();
+		search = new StringBuilder("%").append(search).append("%").toString();
+		cq.where(cb.or(cb.like(computer.get("name").as(String.class), search), cb.like(company.get("name").as(String.class), search)));
+		
+//		if("asc".equals(field))
+//			cq.orderBy(cb.asc(c.get("currency")));
+		
+		Query query = entityManager.createQuery(cq);
+		
+		//Predicate namePredicate = cb.like(computer.get("name"), searchParamExp);
+		return null;
 	}
 
 	/**
@@ -81,9 +107,20 @@ public class ComputerDAO implements IComputerDAO {
 	 * Retrieves number of results for research
 	 */
 	public long readTotal(String search) {
-		return (Long)entityManager.createQuery("SELECT COUNT(computer) FROM Computer as computer LEFT JOIN computer.company company WITH company.name LIKE :search WHERE computer.name LIKE :search")
-				.setParameter("search", "%" + search + "%")
-				.getSingleResult();
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<Computer> computer = cq.from(Computer.class);
+		cq.select(cb.count(computer));
+
+		Join<Computer, Company> company = computer.join("company", JoinType.LEFT);
+
+		search = new StringBuilder("%").append(search).append("%").toString();
+		cq.where(cb.or(cb.like(computer.get("name").as(String.class), search), cb.like(company.get("name").as(String.class), search)));
+
+		Query query = entityManager.createQuery(cq);
+
+		return ((Long)query.getSingleResult()).intValue();
 	}
 
 	public long create(Computer myComp) {

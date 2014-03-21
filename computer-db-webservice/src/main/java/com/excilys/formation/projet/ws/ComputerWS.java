a3b8f7c64e61ff7838eda8dae7fe1d2e9b33f524
Alexandre Nunesse <anunesse@excilys.com>
2014-03-21 18:04:48 +0100

    Publishing Jax-RS v3.8.0

--- computer-db-webservice/src/main/java/com/excilys/formation/projet/ws/ComputerWS.java
+++ computer-db-webservice/src/main/java/com/excilys/formation/projet/ws/ComputerWS.java
@@ -2,10 +2,19 @@ package com.excilys.formation.projet.ws;
 
 import java.util.List;
 
-import javax.jws.WebService;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-@WebService
+@Consumes("application/json")
+@Produces("application/json")
 public interface ComputerWS {
+	@POST
+	@Path("/readAll/")
 	public List<String> readAll();
+	
+	@POST
+	@Path("/helloWorld/")
 	public String helloWorld();
 }
--- computer-db-webservice/src/main/java/com/excilys/formation/projet/ws/impl/ComputerWSImpl.java
+++ computer-db-webservice/src/main/java/com/excilys/formation/projet/ws/impl/ComputerWSImpl.java
@@ -3,8 +3,6 @@ package com.excilys.formation.projet.ws.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.jws.WebService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +10,13 @@ import com.excilys.formation.projet.om.domain.Computer;
 import com.excilys.formation.projet.service.ComputerService;
 import com.excilys.formation.projet.ws.ComputerWS;
 
-@WebService(endpointInterface="com.excilys.formation.projet.ws.ComputerWS")
+
 @Service("computerWS")
 public class ComputerWSImpl implements ComputerWS{
 
 	@Autowired
 	ComputerService computerService;
 	
-	@Override
 	public List<String> readAll() {
 		List<String> myList = new ArrayList<String>();
 		for(Computer c : computerService.read(0, 20, "ASC", "computer.name", "")){
@@ -28,7 +25,6 @@ public class ComputerWSImpl implements ComputerWS{
 		return myList;
 	}
 
-	@Override
 	public String helloWorld() {
 		return "Hello Dude!";
 	}
